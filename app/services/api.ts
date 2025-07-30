93400625805b7d013855f2a8645e513fe27299d5
Tombunzel <bunzel.tom@gmail.com>
2025-07-30 21:30:01 +0200

    chore(*): clean up for empty branch

--- app/services/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-// We define the base URL of our backend API.
-export const api = axios.create({
-  baseURL: 'http://localhost:3000', // Your Fastify backend address
-});
